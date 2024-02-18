3d937a327ba2ca922f52ab7fd0823f4ca7b94c8f
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-02-18 14:33:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var friends = ["Huzaifa", "Asad", "Aryan", "Muzamil"];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
