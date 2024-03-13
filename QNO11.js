54194ba3579e905e654ca991c62f7209d4a2ea82
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:20:26 +0500

    Add files via upload

---
+++ QNO11.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let friends = ["Huzaifa", "Asad", "Aryan", "Muzamil"];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
