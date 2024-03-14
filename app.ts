2f5d98f884a32203530816e31a6fac0613996711
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:14:28 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let friends : string[]=["Huzaifa", "Asad", "Aryan","Muzamil"];
-console.log(friends[0]);
-console.log(friends[1]);
-console.log(friends[2]);
-console.log(friends[3]);
