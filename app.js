db86301d900ace43a0d127a5ad04549e13b241d9
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:14:19 +0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let friends = ["Huzaifa", "Asad", "Aryan", "Muzamil"];
-console.log(friends[0]);
-console.log(friends[1]);
-console.log(friends[2]);
-console.log(friends[3]);
